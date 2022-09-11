58bdec438ebcf61af13bf1257b30f458c5be85bb
CYBERDEVILZ <pranavcosmos4@gmail.com>
2022-09-11 20:04:57 +0530

    initial commit

---
+++ flask-nft.py
@@ -0,0 +1,160 @@
+from flask import Flask, render_template, request, session, redirect
+import requests, json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from firebase_admin import auth
+from web3 import Web3
+
+# FIREBASE CONFIG
+cred = credentials.Certificate(r'env\nft-marketplace-mustb2b-firebase-adminsdk-uu5ru-78e423d239.json')
+default_app = firebase_admin.initialize_app(cred, {'storageBucket': 'nft-marketplace-mustb2b.appspot.com'})
+db = firestore.client()
+
+# FLASK CONFIG
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "nft-marketplace-mustb2b"
+
+# WEB3 CONFIG
+web3 = Web3(Web3.HTTPProvider("https://polygon-mumbai.infura.io/v3/649dce30cf974f4a9ad9329bcb7fb59a"))
+print(web3.isConnected())
+
+# PINATA CONFIG
+url = "https://api.pinata.cloud/data/testAuthentication"
+headers = { 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJlYzgzNmI0OC05NTNlLTQ2ZWYtODkzMy1mMDU0NjYwMjQ2ZTYiLCJlbWFpbCI6InByYW5hdmNvc21vczRAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInBpbl9wb2xpY3kiOnsicmVnaW9ucyI6W3siaWQiOiJGUkExIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9LHsiaWQiOiJOWUMxIiwiZGVzaXJlZFJlcGxpY2F0aW9uQ291bnQiOjF9XSwidmVyc2lvbiI6MX0sIm1mYV9lbmFibGVkIjpmYWxzZSwic3RhdHVzIjoiQUNUSVZFIn0sImF1dGhlbnRpY2F0aW9uVHlwZSI6InNjb3BlZEtleSIsInNjb3BlZEtleUtleSI6ImYzMmVmMDY0MzZmMzVlMjA2ZGJkIiwic2NvcGVkS2V5U2VjcmV0IjoiMWYxMTY0OTliYjVmZTM2OGUwMDk2Y2ZjNWIxOWQyOWNmZjI5ZTlhMzU2MTZlMDljZjNlMzY2M2U0ZjcyYWM3ZiIsImlhdCI6MTY2MjgzMzg5N30.l-2R-1EiPyLVPQDGd3acostkRMuNCFW9am1byyYvHNo' }
+
+
+# DEFAULT
+@app.route("/")
+def selectUserType():
+    return render_template("selectusertype.html")
+
+# LOGIN
+@app.route("/<select>/login", methods=["GET", "POST"])
+def login(select):
+    if session.get("uid") != None:
+        return redirect("/selectbooth")
+    if session.get("cid") != None:
+        return redirect("/metaverse")
+    if request.method == "GET":
+        return render_template("login.html", type=select)
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        try:
+            response = requests.post(
+                url="https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBwVRHYyOnRC3zNdHfxzq8kjhaI7rTiHGI", 
+                data={"email":email,"password":password,"returnSecureToken":True}
+            )
+            idToken = json.loads(response.text)["idToken"]
+            expiresIn = json.loads(response.text)["expiresIn"]
+            print(response.text)
+            uid = json.loads(response.text)["localId"]
+            if select == "booth":
+                session["uid"] = uid
+                return redirect("/selectbooth")
+            if select == "customer":
+                session["cid"] = uid
+                return redirect("/metaverse")
+            else:
+                return redirect("/")
+
+        except:
+            return redirect("/")
+
+# SIGNUP
+@app.route("/<select>/signup", methods=["GET", "POST"])
+def signup(select):
+    if request.method == "GET":
+        return render_template("signup.html")
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        try:
+            user = auth.create_user(email=email, password=password)
+            if select == "booth":
+                session["uid"] = user.uid    
+                return redirect("/selectbooth")
+            if select == "customer":
+                session["cid"] = user.uid
+                return redirect("/metaverse")
+            else:
+                return redirect("/")
+        except:
+            return "Failed to create user"
+
+# SIGNOUT
+@app.route("/signout")
+def signout():
+    try:
+        session.pop("uid")
+        session.pop("cid")
+    except:
+        pass
+    return redirect("/")
+
+# SELECT BOOTH
+@app.route("/selectbooth")
+def selectBooth():
+    if session.get("uid") == None:
+        return redirect("/")
+    return render_template("selectbooth.html")
+
+# CONNECT WALLET
+@app.route("/connectWallet")
+def connect():
+    if session.get("uid") == None:
+        return redirect("/")
+    return render_template("connectWallet.html")
+
+# FILE UPLOAD
+@app.route("/createNFT", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "GET":
+        return render_template("createNFT.html")
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            try:
+                # send file to ipfs storage
+                url = "https://api.pinata.cloud/pinning/pinFileToIPFS"
+                files=[
+                ('file',(file.filename,file.read(),'application/octet-stream'))
+                ]
+                response = requests.request("POST", url, headers=headers, files=files)
+                ipfsHash = json.loads(response.text)["IpfsHash"]
+                # send metadata to contract
+                if ipfsHash != None:
+                    print (f"send the metadata like name, description, IPFSURI: https://ipfs.filebase.io/ipfs/{ipfsHash}, price to contract to let it store there")
+                    return redirect("/nftbooth")
+            except:
+                return "awww snap! something went wrong"
+    return "awww snap! something went wrong"
+
+
+# NFT BOOTH
+@app.route("/nftbooth")
+def nftbooth():
+    if session.get("uid") == None:
+        return redirect("/")
+    # data = db.collection("nftbooths").document(session["uid"]).get().to_dict()
+    return render_template("nftbooth.html")
+
+# VIEW NFT
+@app.route("/nftbooth/<id>")
+def nftview(id):
+    if session.get("uid") == None:
+        return redirect("/")
+    return render_template("viewnft.html", id=id)
+    
+
+
+if(__name__ == "__main__"):
+    app.run(debug=True)
