230f9efd53395eec3778c9a8cc678b6ed4346f58
CYBERDEVILZ <pranavcosmos4@gmail.com>
2022-09-15 18:19:45 +0530

    style: connectwallet

--- static/web3Connect.js
+++ static/web3Connect.js
@@ -573,7 +573,8 @@ console.log("contract deployed at: "+addressContract)
 // script to access metamask wallet
 access = async () => {
     window.web3 = new Web3(window.ethereum);
-    await window.web3.eth.getAccounts().then((success) => {walletAddress = success[0]; console.log(success[0])});
+    await window.web3.eth.getAccounts().then((success) => {walletAddress = success[0]; console.log(success[0])}, (failure) => {
+		walletAddress = null;}).catch((e)=>{walletAddress = null;});
 }
 
 // minting fee
